8968d33b0561f29a0778e3ca477ad41a6b104344
Gonduls <pierluigi.negro.00@gmail.com>
2021-10-15 20:10:22 +0200

    day one complete

---
+++ src/day01/part1.java
@@ -0,0 +1,26 @@
+package day01;
+
+import java.io.File;
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner;
+
+public class part1 {
+    public static void main(String[] args) {
+        int result = 0;
+        try {
+            File input = new File("AdventOfCode2019/src/day01/input.txt");
+            Scanner scanner = new Scanner(input);
+            while(scanner.hasNextInt()){
+                //System.out.println( scanner.nextInt());
+                result += scanner.nextInt() / 3 - 2;
+            }
+            scanner.close();
+
+        } catch(FileNotFoundException e) {
+            System.out.println("Unable to open file.");
+            //e.printStackTrace();
+        }
+
+        System.out.println(result);
+    }
+}
---
+++ src/day01/part2.java
@@ -0,0 +1,33 @@
+package day01;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class part2 {
+    public static void main(String[] args) {
+        int result = 0;
+        try {
+            File input = new File("AdventOfCode2019/src/day01/input.txt");
+            Scanner scanner = new Scanner(input);
+            while(scanner.hasNextInt()){
+                //System.out.println( scanner.nextInt());
+                result += totalFuel(scanner.nextInt());
+            }
+            scanner.close();
+
+        } catch(FileNotFoundException e) {
+            System.out.println("Unable to open file.");
+            //e.printStackTrace();
+        }
+
+        System.out.println(result);
+    }
+
+    static private int totalFuel(int x){ // had to be static in order to be accessible from static main
+        if (x<6)
+            return 0;
+        int result = x/3 -2;
+        return(result + totalFuel(result));
+    }
+}
